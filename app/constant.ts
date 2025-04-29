f285bd69c3cb397934ca2496a89e0eed36953fc4
Wxh <wxhmessage@live.cn>
2025-04-29 11:31:31 +0800

    googleModels增加"gemini-2.5-pro-exp-03-25"

--- app/constant.ts
+++ app/constant.ts
@@ -547,6 +547,7 @@ const googleModels = [
   "gemini-2.0-pro-exp",
   "gemini-2.0-pro-exp-02-05",
   "gemini-2.5-flash-preview-04-17",
+  "gemini-2.5-pro-exp-03-25",
 ];
 
 const anthropicModels = [
