3b4d2e5e00f526aabd4171d845528f7643df71bb
wxh <wxhmessage@live.cn>
2025-08-19 12:11:31 +0800

    添加grok-4,DeepSeek-R1,DeepSeek-V3,Doubao-Seed-1.6的支持

--- app/constant.ts
+++ app/constant.ts
@@ -493,7 +493,7 @@ export const VISION_MODEL_REGEXES = [
   /o3/,
   /o4-mini/,
   /grok-4/i,
-  /gpt-5/
+  /gpt-5/,
 ];
 
 export const EXCLUDE_VISION_MODEL_REGEXES = [/claude-3-5-haiku-20241022/];
@@ -561,7 +561,7 @@ const googleModels = [
   "gemini-2.0-pro-exp",
   "gemini-2.0-pro-exp-02-05",
   "gemini-2.5-pro-preview-06-05",
-  "gemini-2.5-pro"
+  "gemini-2.5-pro",
 ];
 
 const anthropicModels = [
@@ -604,6 +604,9 @@ const bytedanceModels = [
   "Doubao-pro-4k",
   "Doubao-pro-32k",
   "Doubao-pro-128k",
+  "DeepSeek-R1",
+  "DeepSeek-V3",
+  "Doubao-Seed-1.6",
 ];
 
 const alibabaModes = [
@@ -673,6 +676,7 @@ const xAIModes = [
   "grok-3-beta",
   "grok-3",
   "grok-3-latest",
+  "grok-4",
 ];
 
 const chatglmModels = [
